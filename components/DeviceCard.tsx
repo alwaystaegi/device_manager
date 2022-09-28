58b61e69d18a5d1d9abad29a65f73554a1df3dc1
alwaystaegi <ffff4565@naver.com>
2022-09-28 18:20:52 +0900

    센서 밸류값을 받아와 장비카드의 밸류로 올리는 기능 추가...?  센서밸류값은 임의로 입력

--- components/DeviceCard.tsx
+++ components/DeviceCard.tsx
@@ -1,17 +1,26 @@
 import { Device } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 interface DeviceCardProps {
   device: Device;
 }
 export default function DeviceCard({ device }: DeviceCardProps) {
-  console.log(device);
+  const [value, setValue] = useState(Number);
+  useEffect(() => {
+    fetch(`api/sencing/${device.id}`)
+      .then((res) => res.json())
+      .then((json) => setValue(json.result));
+  }, []);
+
   return (
     <div
       key={device.id}
       className="shadow-3d bg-red-200 border-2 w-52 h-52 p-4 flex flex-col justify-between rounded-xl m-5 bg-gradient-to-b from-blue-200 to-cyan-200 hover:from-blue-300 hover:to-cyan-300"
     >
       <div className="flex justify-end">
-        <span className="text-5xl dark:text-gray-500">-</span>
+        <span className="text-5xl dark:text-gray-500">
+          {value ? value : "-"}
+        </span>
         <span className="text-2xl text-gray-500 dark:text-gray-700">
           {device.unit}
         </span>
--- pages/api/sencing/[deviceid].ts
+++ pages/api/sencing/[deviceid].ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Device, DeviceType } from "@prisma/client";
+import { Sencing } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { userAgent } from "next/server";
 import client from "../../../libs/server/client";
 
 // type Data = {
@@ -10,6 +9,7 @@ import client from "../../../libs/server/client";
 interface Data {
   ok: Boolean;
   error?: String;
+  result?: number;
 }
 export default async function handler(
   request: NextApiRequest,
@@ -21,6 +21,7 @@ export default async function handler(
       error: `지원하지 않는 메서드 입니다:${request.method}`,
     });
   }
+
   const { deviceid } = request.query;
   if (!deviceid) {
     response.status(200).json({
@@ -28,8 +29,24 @@ export default async function handler(
       error: `장치 ID(deviceid)를 입력해주세요`,
     });
   }
+  console.log(deviceid);
   try {
-    response.status(200).json({ ok: true });
+    const result = await client.sencing.findFirst({
+      // ? where=== 필터링 ... sencing 안의 요소가 있는 것 들만 검사함
+      where: {
+        deviceId: deviceid?.toString(),
+      },
+      //   ? orderBy 정렬하기 desc 오름차순 asc내림차순
+      orderBy: {
+        createAt: "desc",
+      },
+      //   ?select 선택한당
+      select: {
+        value: true,
+      },
+    });
+    console.log(result);
+    response.status(200).json({ ok: true, result: result?.value });
   } catch (err) {
     response.status(200).json({ ok: false, error: `${err}` });
   }
