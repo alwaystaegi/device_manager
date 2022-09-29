8085d247d19bd63a2c8b4532005e5d4e1ccb1c2d
alwaystaegi <ffff4565@naver.com>
2022-09-29 16:39:21 +0900

    그림자 삭제... ㅠㅠ

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      boxShadow: {
-        "3d": "rgba(0, 0, 0, 0.17) 0px -23px 15px 0px inset, rgba(0, 0, 0,0.05) 0px -15px 30px 15px inset, rgba(0, 0, 0, 0.001) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.06) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 18px 16px;",
-        test: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-      },
-      translate: {
-        a: "5 1s linear",
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     boxShadow: {
+  //       "3d": "rgba(0, 0, 0, 0.17) 0px -23px 15px 0px inset, rgba(0, 0, 0,0.05) 0px -15px 30px 15px inset, rgba(0, 0, 0, 0.001) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.06) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 18px 16px;",
+  //       test: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+  //     },
+  //     translate: {
+  //       a: "5 1s linear",
+  //     },
+  //   },
+  // },
   plugins: [],
   darkMode: "class",
 };
